eec3386aa3702e9162155ed0c65516838a86a28b
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-03-22 12:10:23 +0400

    Updated JS Task 5

--- jsTasks/task5.js
+++ jsTasks/task5.js
@@ -1,9 +1,15 @@
 const car = {
+  _isChangedYear: false,
   _year: null,
   brand: "Toyota",
 
   set year(newYear) {
-    if (this._year === null && typeof newYear === "number" && newYear >= 1886) {
+    if (
+      !this._isChangedYear &&
+      typeof newYear === "number" &&
+      newYear >= 1886
+    ) {
+      this._isChangedYear = true;
       this._year = newYear;
       return;
     }
@@ -21,6 +27,17 @@ const car = {
     return currentYear - this._year;
   },
 };
+
+Object.defineProperties(car, {
+  _isChangedYear: {
+    enumerable: false,
+    configurable: false,
+  },
+  _year: {
+    enumerable: false,
+    configurable: false,
+  },
+});
 
 car.year = 2020;
 console.log(car.year);
